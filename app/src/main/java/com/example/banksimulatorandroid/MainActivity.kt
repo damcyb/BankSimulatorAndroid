28889f8ac630e2a6d2191622f2d18462314997bd
FliCom <flicom@MacBook-Pro-FliCom.local>
2019-12-31 15:04:33 +0100

    introScreen

--- app/src/main/java/com/example/banksimulatorandroid/MainActivity.kt
+++ app/src/main/java/com/example/banksimulatorandroid/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.example.banksimulatorandroid
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
+import co.metalab.asyncawait.async
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        async {
+            val result = await {
+                showIntroScreen()
+            }
+            moveToLoginActivity()
+        }
+    }
+
+    private fun showIntroScreen() {
+
+        var progressStatus = 0
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
+        progressBar.progressDrawable.setColorFilter(Color.argb(100, 77, 66, 43)
+            , android.graphics.PorterDuff.Mode.SRC_IN)
+
+        while (progressStatus < 100) {
+            progressStatus++
+            android.os.SystemClock.sleep(50)
+            progressBar.progress = progressStatus
+        }
+    }
+
+    private fun moveToLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java).apply {
+            //putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
