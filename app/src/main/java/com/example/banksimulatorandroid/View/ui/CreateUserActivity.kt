11389b92bd1079280148476354d71276b8120d71
FliCom <flicom@MacBook-Pro-FliCom.local>
2020-01-03 00:40:09 +0100

    nav drawer header texview added

--- app/src/main/java/com/example/banksimulatorandroid/View/ui/CreateUserActivity.kt
+++ app/src/main/java/com/example/banksimulatorandroid/View/ui/CreateUserActivity.kt
@@ -1,11 +1,15 @@
 package com.example.banksimulatorandroid.View.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProviders
+import com.example.banksimulatorandroid.Constants.EXTRA_USER_REST
+import com.example.banksimulatorandroid.Model.Response.UserRest
 import com.example.banksimulatorandroid.R
 import com.example.banksimulatorandroid.ViewModel.CreateUserViewModel
 import com.example.banksimulatorandroid.ViewModel.LoginViewModel
@@ -26,10 +30,25 @@ class CreateUserActivity : AppCompatActivity() {
         val password = findViewById<TextView>(R.id.passwordTxt)
         val createUserBtn = findViewById<Button>(R.id.createUserBtn)
 
+        observeViewModel()
+
         createUserBtn.setOnClickListener {
             viewModel.createUser(firstName.text.toString(), lastName.text.toString(),
                 email.text.toString(), password.text.toString())
-            //move to another activity
         }
+    }
+
+    private fun observeViewModel() {
+        viewModel.userRest.observe(this, Observer { user ->
+            user?.let {
+                moveToUserAccountActivity(it)
+            }
+        })
+    }
+
+    private fun moveToUserAccountActivity(userAccountDetails: UserRest) {
+        val intent = Intent(this, UserAccountActivity::class.java)
+        intent.putExtra(EXTRA_USER_REST, userAccountDetails)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/banksimulatorandroid/View/ui/UserAccountActivity.kt
+++ app/src/main/java/com/example/banksimulatorandroid/View/ui/UserAccountActivity.kt
@@ -2,8 +2,11 @@ package com.example.banksimulatorandroid.View.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,7 +15,9 @@ import com.example.banksimulatorandroid.Model.Response.UserRest
 import com.example.banksimulatorandroid.R
 import com.example.banksimulatorandroid.View.Adapter.TransferListAdapter
 import com.example.banksimulatorandroid.ViewModel.UserAccountViewModel
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_user_account.*
+import org.w3c.dom.Text
 import java.lang.StringBuilder
 
 class UserAccountActivity : AppCompatActivity() {
@@ -26,17 +31,36 @@ class UserAccountActivity : AppCompatActivity() {
         setContentView(R.layout.activity_user_account)
         userAccountDetails = intent.getParcelableExtra(EXTRA_USER_REST)
 
+        val header = (findViewById<View>(R.id.nav_view) as NavigationView).getHeaderView(0)
         val balanceTxt = findViewById<TextView>(R.id.balanceTxt)
         val accountNumberTxt = findViewById<TextView>(R.id.accountNumberTxt)
+        val navDrawerNameTxt = header.findViewById<TextView>(R.id.navDrawerNameTxt)
+        val navDrawerEmailTxt = header.findViewById<TextView>(R.id.navDrawerEmailTxt)
 
         val balanceString = "${String.format("%.2f", userAccountDetails.balance)} PLN"
         val accountNumberStringBuilder = StringBuilder()
         accountNumberStringBuilder.append(userAccountDetails.accountNumber)
         accountNumberStringBuilder.insert(2, " ").insert(7, " ")
             .insert(12, " ").insert(17, " ").insert(22, " ").insert(27, " ")
+        val nameString = "${userAccountDetails.firstName} ${userAccountDetails.lastName}"
+        val emailString = "${userAccountDetails.email}"
 
         balanceTxt.text = balanceString
         accountNumberTxt.text = accountNumberStringBuilder
+        navDrawerNameTxt.text = nameString
+        navDrawerEmailTxt.text = emailString
+
+        val toolbar = findViewById<Toolbar>(R.id.actionToolbar)
+        setSupportActionBar(toolbar)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeButtonEnabled(true)
+            actionBar.setDisplayShowTitleEnabled(false)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
+        navDrawerOptionsSelected()
 
         viewModel = ViewModelProviders.of(this).get(UserAccountViewModel(application)::class.java)
         viewModel.refresh(userAccountDetails.userId)
@@ -68,5 +92,62 @@ class UserAccountActivity : AppCompatActivity() {
                 }
             }
         })
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return return when(item.itemId) {
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun navDrawerOptionsSelected() {
+        val navDrawerView = findViewById<NavigationView>(R.id.nav_view)
+        navDrawerView.setNavigationItemSelectedListener { menuItem ->
+
+            if(menuItem.itemId == R.id.depositMoneyItm) {
+                depositMoneyOptionSelected()
+                drawerLayout.closeDrawer(GravityCompat.START)
+            }
+            if(menuItem.itemId == R.id.withdrawMoneyItm) {
+                withdrawMoneyOptionSelected()
+                drawerLayout.closeDrawer(GravityCompat.START)
+            }
+            if(menuItem.itemId == R.id.transferMoneyItm) {
+                transferMoneyOptionSelected()
+                drawerLayout.closeDrawer(GravityCompat.START)
+            }
+            if(menuItem.itemId == R.id.updateUserDataItm) {
+                updateUserDataOptionSelected()
+                drawerLayout.closeDrawer(GravityCompat.START)
+            }
+            if(menuItem.itemId == R.id.deleteAccountItm) {
+                deleteAccountOptionSelected()
+                drawerLayout.closeDrawer(GravityCompat.START)
+            }
+            true
+        }
+    }
+
+    private fun depositMoneyOptionSelected() {
+
+    }
+    private fun withdrawMoneyOptionSelected() {
+
+    }
+
+    private fun transferMoneyOptionSelected() {
+
+    }
+
+    private fun updateUserDataOptionSelected() {
+
+    }
+
+    private fun deleteAccountOptionSelected() {
+
     }
 }
